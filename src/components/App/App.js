4a252813bdc6729d8ff14073fe475ffd168d3c64
Dmitriy Kireev <chief_programmer@mail.ru>
2018-07-29 17:48:58 +0300

    project start

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import VideoPlayer from '../VideoPlayer';
+
+class App extends Component {
+  render() {
+    return <VideoPlayer />;
+  }
+}
+
+export default App;
